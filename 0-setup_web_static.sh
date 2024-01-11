fbf5d8bf890b5e6447886807773899b73e1d144d
Abderrahmane369 <cszamasu@gmail.com>
2024-01-11 01:01:00 +0100

    Updated_Something

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 #iwaywa
 sudo apt-get -y upgrade
-sudo atp-get install -y nginx
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
+sudo apt-get install -y nginx
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 echo "<html>
@@ -13,16 +10,17 @@ echo "<html>
   <body>
     HOLEBRITONIA School
   </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-echo "server {
-				listen 80;
+echo "
+server {
+	listen 80;
 
-				root /var/www/html;
+	root /var/www/html;
 				
-				location /hbnb_statc/ {
-								alias /data/web_static/current/;
-				}
-}" | sudo tee "/etc/nginx/sites-available/default"
+	location /hbnb_static/ {
+				alias /data/web_static/current/;
+	}
+}" | sudo tee "/etc/nginx/sites-enabled/default"
 service nginx restart

051aa12f1d692f8105b147dbbd5419861300a6be
Abderrahmane369 <cszamasu@gmail.com>
2024-01-12 00:17:34 +0100

    Updated_Something

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,36 +6,36 @@ from os.path import isfile
 
 env.hosts = ['54.144.144.29', '54.158.205.242']
 
+
 def do_deploy(archive_path):
     """azeaeze"""
     if not isfile(archive_path):
         return False
 
-    try:
-        put(archive_path, '/tmp/')
-        run('rm -rf /data/web_static/releases/*')
-        run('mkdir -p /data/web_static/releases/{}/'.format(
-            archive_path.split("/")[1][:-4]))
+    a = put(archive_path, '/tmp/')
+    b = run('rm -rf /data/web_static/releases/*')
+    c = run('mkdir -p /data/web_static/releases/{}/'.format(
+        archive_path.split("/")[1][:-4]))
 
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(
-            archive_path.split("/")[1],
-            archive_path.split("/")[1][:-4]
-        ))
-        run(f'rm -rf /tmp/{archive_path.split("/")[1]}')
-        run('mv {}{}/web_static/* {}{}/'.format(
-            '/data/web_static/releases/',
-            archive_path.split("/")[1][:-4],
-            '/data/web_static/releases/',
-            archive_path.split("/")[1][:-4]
-        ))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(
-            archive_path.split("/")[1][:-4]
-        ))
-        run('rm -rf /data/web_static/current')
-        run('ln -sf /data/web_static/releases/{} {}'.format(
-            archive_path.split("/")[1][:-4], '/data/web_static/current'))
+    d = run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(
+        archive_path.split("/")[1],
+        archive_path.split("/")[1][:-4]
+    ))
+    e = run(f'rm -rf /tmp/{archive_path.split("/")[1]}')
+    f = run('mv {}{}/web_static/* {}{}/'.format(
+        '/data/web_static/releases/',
+        archive_path.split("/")[1][:-4],
+        '/data/web_static/releases/',
+        archive_path.split("/")[1][:-4]
+    ))
+    g = run('rm -rf /data/web_static/releases/{}/web_static'.format(
+        archive_path.split("/")[1][:-4]
+    ))
+    h = run('rm -rf /data/web_static/current')
+    k = run('ln -sf /data/web_static/releases/{} {}'.format(
+        archive_path.split("/")[1][:-4], '/data/web_static/current'))
 
-    except Exception:
-        return False
+    f0 = a.failed or b.failed or c.failed or d.failed or e.failed
+    f1 = f.failed or g.failed or h.failed or k.failed
 
-    return True
+    return False if f1 else True

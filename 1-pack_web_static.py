14ceb37f5ebdd9e4e6ecc7d059884952bd7edbcd
Abderrahmane369 <cszamasu@gmail.com>
2024-01-11 18:22:39 +0100

    Updated_Something

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,6 +8,8 @@ def do_pack():
     dt = dtime.now()
 
     local("mkdir -p versions")
-    local("tar -czvf versions/web_static_{}{}{}{}{}{}.tgz web_static".format(
+    r =local("tar -czvf versions/web_static_{}{}{}{}{}{}.tgz web_static".format(
         dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second
     ))
+
+    return None if r.failed else None

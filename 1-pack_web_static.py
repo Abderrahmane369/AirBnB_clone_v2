885517ecf6ca114eb6cb296186c728710f94018f
Abderrahmane369 <cszamasu@gmail.com>
2024-01-11 18:13:20 +0100

    Updated_Something

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,7 +7,8 @@ from datetime import datetime as dtime
 
 def do_pack():
     dt = dtime.now()
-
-    local("tar -czvf versions/web_static_{}{}{}{}{}{}".format(
+    
+    local("mkdir -p versions")
+    local("tar -czvf versions/web_static_{}{}{}{}{}{}.tgz web_static".format(
         dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second
     ))

3471a7f39a7313c7a6cc6f218f04dbf95dd8360b
Abderrahmane369 <cszamasu@gmail.com>
2024-01-12 01:58:19 +0100

    Updated_Something

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive fF"""
-from fabric.api import env, put, run
-from datetime import datetime as dtime
-from os.path import isfile
+"""
+This is the 2-do_deploy_web_static.py module.
+This module distribute the static content (html, css, images) to the servers
+"""
 
-env.hosts = ['54.144.144.29', '54.158.205.242']
+
+from fabric.api import put, run, env, task
+import os
+env.hosts = ['52.91.182.154', '34.202.164.102']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
-    """azeaeze"""
-    if not isfile(archive_path):
+    """ This is the function for deploying the content """
+    if os.path.exists(archive_path) is False:
         return False
     try:
-        tgz = archive_path.split("/")[1]
-        fname = tgz[:-4]
-        put(archive_path, '/tmp/')
-        run('rm -rf /data/web_static/releases/*')
-        run('mkdir -p /data/web_static/releases/' +
-            fname + '/')
-        run('tar -xzf /tmp/' + tgz +
-            ' -C /data/web_static/releases/' + fname + '/')
-        run('rm -rf /tmp/' + tgz)
-        run('mv /data/web_static/releases/' + fname +
-            'web_static/* /data/web_static/releases/'
-            + fname + '/')
-        run('rm -rf /data/web_static/releases/' + fname
-            + '/web_static')
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/' + fname +
-            '/ /data/web_static/current')
+        p = "/data/web_static/releases/"
+        file_name = archive_path.split('/')[1]
+        no_ext = file_name.split('.')[0]
+        sym_link = "/data/web_static/current"
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/releases/" + no_ext + "/")
+        run("tar -xzf /tmp/" + file_name + " -C " + p + no_ext + "/")
+        run("rm -rf /tmp/" + file_name)
+        run("mv " + p + no_ext + "/web_static/* " + p + no_ext + "/")
+        run("rm -rf /tmp/" + file_name)
+        run("rm -rf /data/web_static/releases/" + no_ext + "/web_static")
+        run("rm -rf " + sym_link)
+        run("ln -s /data/web_static/releases/" + no_ext + "/ " + sym_link)
         return True
     except Exception:
         return False

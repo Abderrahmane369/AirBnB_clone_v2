d0b09ba80d4253ce616120fa8160be92262f3ae8
Abderrahmane369 <cszamasu@gmail.com>
2024-01-12 16:25:35 +0100

    Updated_Something

---
+++ 100-clean_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""logarthim"""
+from fabric.api import local, run, env
+from datetime import datetime as dtime
+from re import search as srch
+
+env.hosts = ['54.158.205.242', '54.144.144.29']
+
+
+def do_clean(number=0):
+    vers = local('ls -tr versions', capture=True)
+
+    number = int(number)
+
+    if number == 0:
+        number = 1
+
+    def istgz(e): return srch('web_static_\d+\.tgz', e) != -1
+
+    archives = [_ for _ in vers.stdout.split('\n') if istgz(_)]
+
+    if number > len(archives) or number < 0:
+        return
+
+    for _ in archives[:-number]:
+        local(f'rm -rf versions/{_}')
+        run(f'rm -rf /data/web_static/releases/{_}')

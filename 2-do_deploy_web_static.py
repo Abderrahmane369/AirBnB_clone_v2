f0f8344bf70fba7851dad72b8a78f84d35c585e6
Abderrahmane369 <cszamasu@gmail.com>
2024-01-12 01:14:22 +0100

    Updated_Something

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,28 +12,23 @@ def do_deploy(archive_path):
     if not isfile(archive_path):
         return False
 
+    tgz = archive_path.split("/")[1]
+    fname = tgz[:-4]
+
     a = put(archive_path, '/tmp/')
     b = run('rm -rf /data/web_static/releases/*')
-    c = run('mkdir -p /data/web_static/releases/{}/'.format(
-        archive_path.split("/")[1][:-4]))
+    c = run('mkdir -p /data/web_static/releases/' + fname + '/')
+    d = run('tar -xzf /tmp/' + tgz + ' -C /data/web_static/releases/' + fname
+            + '/')
 
-    d = run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(
-        archive_path.split("/")[1],
-        archive_path.split("/")[1][:-4]
-    ))
-    e = run(f'rm -rf /tmp/{archive_path.split("/")[1]}')
-    f = run('mv {}{}/web_static/* {}{}/'.format(
-        '/data/web_static/releases/',
-        archive_path.split("/")[1][:-4],
-        '/data/web_static/releases/',
-        archive_path.split("/")[1][:-4]
-    ))
-    g = run('rm -rf /data/web_static/releases/{}/web_static'.format(
-        archive_path.split("/")[1][:-4]
-    ))
+    e = run('rm -rf /tmp/' + tgz)
+    f = run('mv /data/web_static/releases/' + fname +
+            '/web_static/* /data/web_static/releases/' + fname + '/')
+
+    g = run('rm -rf /data/web_static/releases/' + fname + '/web_static')
     h = run('rm -rf /data/web_static/current')
-    k = run('ln -sf /data/web_static/releases/{} {}'.format(
-        archive_path.split("/")[1][:-4], '/data/web_static/current'))
+    k = run('ln -s /data/web_static/releases/' + fname +
+            '/ /data/web_static/current')
 
     f0 = a.failed or b.failed or c.failed or d.failed or e.failed
     f1 = f.failed or g.failed or h.failed or k.failed

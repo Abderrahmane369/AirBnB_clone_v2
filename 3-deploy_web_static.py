5322cf2ff101e9a47940bd0468a1a114e8ca7a93
Abderrahmane369 <cszamasu@gmail.com>
2024-01-12 03:00:47 +0100

    Updated_Something

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,6 +4,7 @@ from fabric.api import put, run, env, local
 import os
 env.hosts = ['54.144.144.29', '54.158.205.242']
 
+
 def do_pack():
     """azeaeze"""
     dt = dtime.now()
@@ -17,7 +18,38 @@ def do_pack():
 
     return path if r.failed else None
 
+
+def do_deploy(archive_path):
+    """azeaeze"""
+    if not isfile(archive_path):
+        return False
+    try:
+        tgz = archive_path.split("/")[1]
+        fname = tgz[:-4]
+        put(archive_path, '/tmp/')
+        run('rm -rf /data/web_static/releases/*')
+        run('mkdir -p /data/web_static/releases/' +
+            fname + '/')
+        run('tar -xzf /tmp/' + tgz +
+            ' -C /data/web_static/releases/' + fname + '/')
+        run('rm -rf /tmp/' + tgz)
+        run('mv /data/web_static/releases/' + fname +
+            'web_static/* /data/web_static/releases/'
+            + fname + '/')
+        run('rm -rf /data/web_static/releases/' + fname
+            + '/web_static')
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/' + fname +
+            '/ /data/web_static/current')
+        return True
+    except Exception:
+        return False
+
+
 def deploy():
     archive = do_pack()
-    
-    
+
+    if archive is None:
+        return False
+
+    return do_deploy(archive)

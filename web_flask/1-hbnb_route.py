baac1c257e3da2ea7ff362722c6fcf5fee0f1f8c
Abderrahmane369 <cszamasu@gmail.com>
2024-01-26 16:14:23 +0100

    Updated_Something

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""azeaeaeaeaeae"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """aheloow"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello():
+    """aheloow"""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run()

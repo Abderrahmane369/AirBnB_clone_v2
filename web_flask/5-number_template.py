d3009e51c510351ae70d51160f6c69a350265784
Abderrahmane369 <cszamasu@gmail.com>
2024-01-26 21:52:30 +0100

    Updated_Something

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""azeaeaeaeaeae"""
+from flask import Flask, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """aheloow"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """aheloow"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def txt(text):
+    """aheloow"""
+    return "C " + escape(text).replace('_', ' ')
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python", strict_slashes=False)
+def python(text='is cool'):
+    """aheloow"""
+    return "Python " + escape(text).replace('_', ' ')
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """aheloow"""
+    return str(n) + " is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """aheloow"""
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run()

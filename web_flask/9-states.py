27a299023149f5e16ba41d25e659f28eff28d0c2
Abderrahmane369 <cszamasu@gmail.com>
2024-01-27 15:39:30 +0100

    Updated_Something

---
+++ web_flask/9-states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""looogarihm"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """aaaaa"""
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """states"""
+    states = storage.all(State)
+    return render_template('7-states_list.html', slist=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states(id=None):
+    """states"""
+    state = None
+    if id:
+        state = storage.all(State).get(f'State.{id}')
+    return render_template('9-states.html', state=state)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

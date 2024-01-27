f5b30dc359ef898a7e2d752aafa2fe619801389b
Abderrahmane369 <cszamasu@gmail.com>
2024-01-27 01:36:59 +0100

    Updated_Something

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,16 +7,16 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """sdqdqsdqd"""
-    return render_template('7-states_list.html', states=storage.all(State).values())
-
-
 @app.teardown_appcontext
 def teardown():
     """aaaaa"""
     storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """sdqdqsdqd"""
+    return render_template('7-states_list.html', states=storage.all(State).values())
 
 
 if __name__ == '__main__':
